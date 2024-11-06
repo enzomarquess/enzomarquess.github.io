be15f76d1e577c1327ba35100a997d9e40e5820f
enzomarquess <160614276+enzomarquess@users.noreply.github.com>
2024-11-06 10:31:35 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+console.log("Olá, nós somos da Nutricale. Calcularemos quantas calorias você deve ingerir diariamente.");
+
+let sexo = parseInt(prompt("Diga o seu sexo, digite 1 para masculino, digite 2 para feminino: "));
+let peso = parseFloat(prompt("Digite seu peso em quilos: "));
+let idade = parseFloat(prompt("Digite sua idade: "));
+let altura = parseFloat(prompt("Digite sua altura em centímetros: "));
+
+let resultadoHomem = (66 + (13.7 * peso) + (5 * altura) - (6.8 * idade));
+let resultadoMulher = (665 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade));
+
+if (sexo === 1) {
+    console.log("Você tem que ingerir", resultadoHomem, "calorias.");
+} else if (sexo === 2) {
+    console.log("Você tem que ingerir", resultadoMulher, "calorias.");
+} else {
+    console.log("Escolha o sexo certo.");
+}
