2dd72d6bc4997200302abc593bab2a0f2c0ff0cc
enzomarquess <160614276+enzomarquess@users.noreply.github.com>
2024-11-11 10:36:37 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -8,10 +8,17 @@ let altura = parseFloat(prompt("Digite sua altura em centímetros: "));
 let resultadoHomem = (66 + (13.7 * peso) + (5 * altura) - (6.8 * idade));
 let resultadoMulher = (665 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade));
 
+let RespostaHomem = "o ideal é ingerir  " + resultadoHomem 
+let RespostaMulher = "o ideal é ingerir " + resultadoMulher
+
 if (sexo === 1) {
     console.log("Você tem que ingerir", resultadoHomem, "calorias.");
 } else if (sexo === 2) {
     console.log("Você tem que ingerir", resultadoMulher, "calorias.");
 } else {
     console.log("Escolha o sexo certo.");
+}
+
+function calcularIMC(){
+    document.getElementById("resposta").innerHTML = RespostaHomem
 }
